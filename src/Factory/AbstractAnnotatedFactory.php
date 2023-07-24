bbeea3f4627db08f20be0051e8b4644d0101019a
Andrei Onita <oni@rospace.com>
2023-07-24 11:10:11 +0300

    AbstractAnnotatedFactory::CACHE_SERVICE restored to public

--- src/Factory/AbstractAnnotatedFactory.php
+++ src/Factory/AbstractAnnotatedFactory.php
@@ -15,7 +15,7 @@ use Psr\Container\NotFoundExceptionInterface;
 
 abstract class AbstractAnnotatedFactory
 {
-    protected const CACHE_SERVICE       = 'Dot\AnnotatedServices\Cache';
+    public const CACHE_SERVICE          = 'Dot\AnnotatedServices\Cache';
     protected ?Reader $annotationReader = null;
     public function setAnnotationReader(Reader $annotationReader): void
     {
